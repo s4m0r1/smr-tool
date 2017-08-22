3c306b6d33b56fec5a93e5efbdd7198f7713307c
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2017-08-22 10:54:57 +0900

    aiueo

---
+++ test/sample_nural.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import keras
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import RMSprop
+
+#Kerasの関数でデータの読み込み。データをシャッフルして学習データと訓練データに分割してくれる
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+num_classes = 10
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+y_train = y_train.astype('int32')
+y_test = y_test.astype('int32')
+y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
+y_test =  keras.utils.np_utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+batch_size = 128
+epochs = 20
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size, epochs=epochs,
+                    verbose=1, validation_data=(x_test, y_test))
+
+#学習状況可視化
+def plot_history(history):
+    # 精度の履歴をプロット
+    plt.plot(history.history['acc'],"o-",label="accuracy")
+    plt.plot(history.history['val_acc'],"o-",label="val_acc")
+    plt.title('model accuracy')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.legend(loc="lower right")
+    plt.show()
+
+    # 損失の履歴をプロット
+    plt.plot(history.history['loss'],"o-",label="loss",)
+    plt.plot(history.history['val_loss'],"o-",label="val_loss")
+    plt.title('model loss')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(loc='lower right')
+    plt.show()
+# modelに学習させた時の変化の様子をplot
+plot_history(history)
