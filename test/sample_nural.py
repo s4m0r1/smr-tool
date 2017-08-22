328a64b8d4c9047323549d8f0bd932ec9d437625
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2017-08-22 11:19:31 +0900

    エポック更新

--- test/sample_nural.py
+++ test/sample_nural.py
@@ -12,8 +12,8 @@ from keras.optimizers import RMSprop
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 num_classes = 10
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
+x_train = x_train.reshape(1000000, 784)
+x_test = x_test.reshape(100000, 784)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
