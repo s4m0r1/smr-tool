d5b807d895e9220cacdfcbb2f2039c3b3868244a
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2017-09-29 12:22:04 +0900

    auieo

--- setting/install.sh
+++ setting/install.sh
@@ -1,34 +1,24 @@
+echo '#######################'
+echo '#さもりつーずづ初期設定 #'
+echo '#######################'
+echo '#########V0.1##########'
+echo '#######################'
 
-echo Zshellインスコするよ
-'ここでいろいろ聞く'
-'Centならyumでの設定Ubuntuならaptでの設定'
-'' 
+
+'ここでいろいろ聞く cuiだけならターミネータとかいらねぇよな的な'
+'メモ
+zshell nginx php7.0
+Centならyumでの設定Ubuntuならaptでの設定
+どちらにしろリポジトリは追加しておく。'
 echo
 sleep 5
 clear
 echo file setting ...
 sleep 2
-cd ~ 
+'どうせならちゃんとディレクトリを合わせておきたいので~/git/smr/に配置'
+cd ~
 mkdir git
 cd git
 echo git clone smrtools
 git clone github.com/s4m0r1/smr
 cd smr
-git pull
-
-'os ubuntu'
-
-declare -a info=($(./bin/get_os_info))
-
-case ${info[0]} in
-ubuntu)
-    echo "ubuntu"
-
-    if [[ ${info[1]} == "x86_64" ]]; then
-        echo x86_64
-    fi
-    ;;
-*)
-    echo "unsupported"
-    ;;
-esac
