db5860afb40a43b227b6c7c5e7d4dd0bfc3271ad
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2017-10-21 16:33:44 +0900

    owncloud download

--- setting/soft.sh
+++ setting/soft.sh
@@ -1,3 +1,1 @@
-wget -q https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- | sudo apt-key add –
-wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | sudo apt-key add –
-sudo wget https://www.ubuntulinux.jp/sources.list.d/trusty.list -O /etc/apt/sources.list.d/ubuntu-ja.list
+wget http://ftp.icm.edu.pl/packages/owncloud/owncloud-10.0.3.zip
