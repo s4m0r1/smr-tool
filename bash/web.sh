772466448f12d0a9a63946f5dab0d013cf58e301
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2017-12-16 15:55:47 +0900

    auto nginx

--- bash/web.sh
+++ bash/web.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
-
-ssh root@team07-001c
-sleep 100
-x56recz2
+#!/bin/bash
+for NUMBER in `seq -f %03g 3 100`
+                                                                                                                                                                           
+do
+#ssh login
+sshpass -p "x56recz2" ssh -o "StrictHostKeyChecking no" team07-c"${NUMBER}"
+sleep 5s
+sudo bash
+apt update;apt install -y nginx
+cd /var/www/html/
+touch hostname.json
+echo "{"hostname":"team07-c"${NUMBER}""}" > "${NUMBER}".txt
+exit
+logout
+sleep 5s
+echo "####################################################"
+echo "###################"${NUMBER}"######################"
+echo "####################################################"
+done
